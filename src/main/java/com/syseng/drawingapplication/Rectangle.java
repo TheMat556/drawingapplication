4b1616340d34dcff9ea4e560a35178e213ea91b5
TheMat556 <79401695+TheMat556@users.noreply.github.com>
2023-09-26 16:56:35 +0200

    added lombok

---
+++ src/main/java/com/syseng/drawingapplication/Rectangle.java
@@ -0,0 +1,8 @@
+package com.syseng.drawingapplication;
+
+import lombok.Getter;
+
+@Getter
+public class Rectangle {
+    double x;
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.syseng.drawingapplication {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires lombok;
 
     opens com.syseng.drawingapplication to javafx.fxml;
     exports com.syseng.drawingapplication;
